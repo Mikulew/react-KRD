cd2ccc04ee11728f50156e717e3b509009bb2f71
Mikulew <mikulew@gmail.com>
2019-10-28 02:38:10 +0100

    Adding useState to SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../less/components/SearchBar.less';
 
 const SearchBar: React.FC = () => {
+  const [text, setText] = useState('test');
+
+  const search = () => {
+    console.log(text);
+  };
+
   return (
     <>
-      <input type="search" className="search-bar search-bar__input" />
-      <button type="button" className="search-bar search-bar__button">
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+        type="search"
+        className="search-bar search-bar__input"
+      />
+      <button type="button" className="search-bar search-bar__button" onClick={() => search()}>
         szukaj
       </button>
     </>
